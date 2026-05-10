e8b88f736b7013b08c6dee687834ee2c0aa09040
AntoniEntrepreneur <antonipieszczhoch@gmail.com>
2026-05-10 10:29:36 +0200

    Fronend configuration

---
+++ frontend/src/App.jsx
@@ -0,0 +1,8 @@
+function App() {
+  return (
+    <div className="flex items-center justify-center h-screen bg-blue-500">
+      <h1 className="text-white text-5xl font-bold">Tailwind is Working! 🚀</h1>
+    </div>
+  )
+}
+export default App
